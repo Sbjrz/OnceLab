1ed9fab801a117c23b136b241e3c6aaabc881ada
Sbjrz <150453053+Sbjrz@users.noreply.github.com>
2023-11-10 09:59:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import streamlit as st
 
 st.title("Mi primera app")
 
-
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('Imagen app.jpg')
 
-st.image(image, caption='Interfaces multimodales')
+st.image(image, caption='Imagen')
 
 
 texto = st.text_input('Escribe algo', 'Este es mi texto')
