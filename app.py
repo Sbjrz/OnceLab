e1d265219118c609310d878fbca0c6fb76af1774
Sbjrz <150453053+Sbjrz@users.noreply.github.com>
2023-11-10 09:52:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 import streamlit as st
+
+
 st.title("Mi primera app")
+
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
+
+st.image(image, caption='Interfaces multimodales')
+
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)
+
+st.subheader("Ahora usemos 2 Columnas")
+
+col1, col2 = st.columns(2)
